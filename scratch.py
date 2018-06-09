69e39a5b622ffe8438b10ba1147034b68f5071e4
unknown <asharapova19@gmail.com>
2018-06-09 22:21:40 +0300

    бот принимает напоминания

--- scratch.py
+++ scratch.py
@@ -1,18 +1,30 @@
-# -*- coding: utf-8 -*-
 import time
 import vk_api
-vk = vk_api.VkApi(login = 'login', password = 'password')
-vk_api.VkApi(token='7d07fa9396f2a078727fa4faf6b505e6998fdc5a5e2a9f94c064e58674fd36c53da1c473c2affa4b8822b') #Авторизоваться как сообщество
-vk.auth()
+vk = vk_api.VkApi(token = '7d07fa9396f2a078727fa4faf6b505e6998fdc5a5e2a9f94c064e58674fd36c53da1c473c2affa4b8822b')
 values = {'out': 0,'count': 100,'time_offset': 60}
-
 def write_msg(user_id, s):
     vk.method('messages.send', {'user_id':user_id,'message':s})
-
+reminders={"aaidosha": []}
 while True:
     response = vk.method('messages.get', values)
     if response['items']:
         values['last_message_id'] = response['items'][0]['id']
     for item in response['items']:
-            write_msg(item[u'user_id'], u'Привет!')
-    time.sleep(1)
+        if item["body"]=='Напоминание':
+            write_msg(item['user_id'],'А какой текст?')
+            response = vk.method('messages.get', values)
+            while not response['items']:
+                response = vk.method('messages.get', values)
+                if response['items']:
+                    break
+                time.sleep(1)
+            print(response)
+            if response['items']:
+                message = response['items'][0]
+                values['last_message_id'] = message['id']
+                reminders["aaidosha"].append(message['body'])
+                write_msg(item['user_id'], message['body'])
+        else:
+            write_msg(item['user_id'],'Привет!')
+        time.sleep(1)
+
